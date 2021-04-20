a593ac49a9d528dccf9edcfa817fe2fb5e8bbb9e
imjunsiang <neojunsiang@gmail.com>
2021-04-20 20:35:33 +0800

    added console.log for edit and delete

--- client/src/Components/TransactionTable.js
+++ client/src/Components/TransactionTable.js
@@ -12,6 +12,14 @@ const TransactionTable = ({ allTransactions }) => {
     })
     console.log(transaction);
 
+    const handleEdit = () => {
+        console.log("edited txn table");
+    }
+
+    const handleDelete = () => {
+        console.log("deleted txn table");
+    }
+
     const columns = [
         {
             title: "Date",
@@ -40,23 +48,23 @@ const TransactionTable = ({ allTransactions }) => {
             key: "action",
             render: () => (
                 < Space size="middle" >
-                    <Button type="primary">Edit</Button>
-                    <Button type="primary">Delete</Button>
+                    <Button onClick={handleEdit} type="primary">Edit</Button>
+                    <Button onClick={handleDelete} type="primary">Delete</Button>
                 </ Space >
             )
         },
     ]
 
-    const data = [
-        {
-            "date": "5 March 2021",
-            "type": "income",
-            "category": "salary",
-            "amount": "100",
-            "description": "Salary",
-            "bankId": "607e59a3f69d360ef5c7b877"
-        }
-    ]
+    // const data = [
+    //     {
+    //         "date": "5 March 2021",
+    //         "type": "income",
+    //         "category": "salary",
+    //         "amount": "100",
+    //         "description": "Salary",
+    //         "bankId": "607e59a3f69d360ef5c7b877"
+    //     }
+    // ]
 
 
     return (
