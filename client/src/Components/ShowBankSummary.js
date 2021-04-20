4cab8bd43fc2c9ccf9d1b96a0433482504ebfdff
imjunsiang <neojunsiang@gmail.com>
2021-04-20 16:23:09 +0800

    added transaction table

--- client/src/Components/ShowBankSummary.js
+++ client/src/Components/ShowBankSummary.js
@@ -5,15 +5,32 @@ import { Layout } from "antd";
 import { Card } from "antd";
 import { Button } from "antd"
 import { useStateValue } from './StateProvider';
+import TransactionTable from './TransactionTable';
 
 const { Content } = Layout;
 const { Meta } = Card;
 
-const ShowBankSummary = ({ bankName, nickName, balance, id }) => {
+const ShowBankSummary = ({ bankName, nickName, balance, id, allTransactions }) => {
     // const [{ allAccounts }, dispatch] = useStateValue();
     // const accountBalance = allAccounts[allAccounts.findIndex((account) => account.bankName === bankName && account.nickName === nickName)].balance;
-
     const createTransactionLink = `/main/${bankName}/${nickName}/${id}/create-transaction`
+
+    // console.log("bank summary allTxns", allTransactions);
+
+    const totalSum = (allTransactions, transactionType) => {
+        let sum = 0;
+        for (let i = 0; i < allTransactions.length; i++) {
+            console.log(allTransactions[i]);
+            if (allTransactions[i].newTransaction.type === transactionType.toString()) {
+                sum += parseFloat(allTransactions[i].newTransaction.amount);
+            }
+            console.log("sum", sum);
+        }
+        return sum
+    }
+
+    // console.log("income", totalSum(allTransactions, "income"));
+    // console.log("expense", totalSum(allTransactions, "expense"));
 
     return (
         <Content
@@ -26,19 +43,20 @@ const ShowBankSummary = ({ bankName, nickName, balance, id }) => {
         >
             <div className="bank__summary">
                 <Card style={{ width: 240, borderColor: "#d1d1d1", borderRadius: "5px", fontSize: "20px" }}>
-                    <Meta title="Balance" /><br />${balance}
+                    <Meta title="Starting Balance" /><br />${balance}
                 </Card>
-                <Card style={{ width: 240, borderColor: "#d1d1d1", borderRadius: "5px" }}>
-                    <Meta title="Income" />
+                <Card style={{ width: 240, borderColor: "#d1d1d1", borderRadius: "5px", fontSize: "20px" }}>
+                    <Meta title="Income" /><br />${totalSum(allTransactions, "income")}
                 </Card>
-                <Card style={{ width: 240, borderColor: "#d1d1d1", borderRadius: "5px" }}>
-                    <Meta title="Expenses" />
+                <Card style={{ width: 240, borderColor: "#d1d1d1", borderRadius: "5px", fontSize: "20px" }}>
+                    <Meta title="Expenses" /><br />${totalSum(allTransactions, "expense")}
                 </Card>
-                <Card style={{ width: 240, borderColor: "#d1d1d1", borderRadius: "5px" }}>
-                    <Meta title="Transactions" />
+                <Card style={{ width: 240, borderColor: "#d1d1d1", borderRadius: "5px", fontSize: "20px" }}>
+                    <Meta title="Ending Balance" /><br />${balance + totalSum(allTransactions, "income") - totalSum(allTransactions, "expense")}
                 </Card>
                 <Link to={createTransactionLink} ><Button type="primary" shape="round">Create a Transaction</Button></Link>
             </div>
+            <TransactionTable allTransactions={allTransactions} />
         </Content>
     )
 }
---
+++ client/src/Components/TransactionTable.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import "../App.css";
+import { Table, Tag, Space, Button } from 'antd';
+
+
+const TransactionTable = ({ allTransactions }) => {
+
+    console.log("txn table", allTransactions);
+    const transaction = allTransactions.map((txn, id) => {
+        console.log(txn.newTransaction);
+        return txn.newTransaction;
+    })
+    console.log(transaction);
+
+    const columns = [
+        {
+            title: "Date",
+            dataIndex: "date",
+            key: "date",
+        },
+        {
+            title: "Type",
+            dataIndex: "type",
+            key: "type",
+        }, {
+            title: "Category",
+            dataIndex: "category",
+            key: "category",
+        }, {
+            title: "Amount",
+            dataIndex: "amount",
+            key: "amount",
+        }, {
+            title: "Description",
+            dataIndex: "description",
+            key: "description",
+        },
+        {
+            title: "Action",
+            key: "action",
+            render: () => (
+                < Space size="middle" >
+                    <Button type="primary">Edit</Button>
+                    <Button type="primary">Delete</Button>
+                </ Space >
+            )
+        },
+    ]
+
+    const data = [
+        {
+            "date": "5 March 2021",
+            "type": "income",
+            "category": "salary",
+            "amount": "100",
+            "description": "Salary",
+            "bankId": "607e59a3f69d360ef5c7b877"
+        }
+    ]
+
+
+    return (
+        <div>
+            <Table columns={columns} dataSource={transaction} />
+        </div>
+    )
+}
+
+export default TransactionTable
