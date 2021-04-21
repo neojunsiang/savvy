da639e61dedbd530888810437953273c6959d161
imjunsiang <neojunsiang@gmail.com>
2021-04-21 15:33:08 +0800

    added edit button

--- client/src/Components/TransactionTable.js
+++ client/src/Components/TransactionTable.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import "../App.css";
 import { Table, Tag, Space, Button } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+
 
 const TransactionTable = ({ allTransactions }) => {
 
@@ -10,7 +13,14 @@ const TransactionTable = ({ allTransactions }) => {
         console.log("deleted " + id);
         fetch("/transactions/" + id, {
             method: "DELETE"
-        }).then((res) => console.log(res.json()))
+        }).then(res => console.log("resData", res.json()))
+    }
+
+    const handleEdit = (record) => {
+        console.log("handle edit", record);
+        console.log("edited " + record._id);
+
+
     }
 
     const columns = [
@@ -46,7 +56,8 @@ const TransactionTable = ({ allTransactions }) => {
                 // console.log(record) // show the object
                 // console.log(index) // show the index of the row 
                 <Space Space size="middle" >
-                    <Button type="primary" onClick={() => handleDelete(record._id)}>Delete</Button>
+                    <Button type="primary" shape="round" onClick={() => handleDelete(record._id)} icon={<DeleteOutlined />} />
+                    <Link to="./EditTransaction.js"><Button type="primary" shape="round" onClick={() => handleEdit(record)} icon={<EditOutlined />} /></Link>
                 </Space >
             )
         },
